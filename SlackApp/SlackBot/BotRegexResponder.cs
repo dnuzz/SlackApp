afb983f1350e07507b4b67d84ebfd778a1525ce3
dnuzzomueller <dnuzzomueller@cimpress.com>
2018-04-30 10:09:31 -0400

    Adding Celcius conversion

--- SlackApp/SlackBot/BotRegexResponder.cs
+++ SlackApp/SlackBot/BotRegexResponder.cs
@@ -1,5 +1,6 @@
 ﻿using SlackAPI;
 using SlackAPI.WebSocketMessages;
+using SlackApp.SlackBot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,22 @@ namespace SlackApp.Controllers
 {
     public class BotRegexResponder
     {
-        public Dictionary<string, Action<NewMessage>> methodMap = new Dictionary<string, Action<NewMessage>>();
+        public static Dictionary<string, Action<NewMessage,string>> methodMap = new Dictionary<string, Action<NewMessage,string>>();
+        public static List<RegexResponse> regexResponses = new List<RegexResponse>();
         private SlackSocketClient client;
 
         public BotRegexResponder(SlackSocketClient client)
         {
             this.client = client;
             client.OnMessageReceived += (message) => { this.Receiver(message); };
-            client.OnMessageReceived += (message) => { Console.WriteLine(message.text); };
-            methodMap.Add("([+-]?\\d+(\\.\\d+)*)\\s?°?([Cc]|(Celcius))", (m) => { TemperatureConversion(m); });
+            client.OnMessageReceived += (message) => { Console.WriteLine(message.text); }; //Remove this later
+            methodMap.Add(@"([+-]?(\d+\.?\d+?)+)\s?°?[Cc]", (m,s) => { TemperatureConversion(m,s); });
+            methodMap.Add(@"([+-]?(\d+\.?\d?)+)\s?([kK]m)|([kK]ilometers)", (m,s) => { DistanceConversion(m,s); });
+        }
+
+        public static void AddRegexResponse(RegexResponse regexResp)
+        {
+            regexResponses.Add(regexResp);
         }
 
         public void Receiver(NewMessage message)
@@ -27,27 +35,47 @@ namespace SlackApp.Controllers
             {
                 if (Regex.IsMatch(message.text,m.Key) && message.subtype != "bot_message")
                 {
-                    m.Value.Invoke(message);
+                    try
+                    {
+                        m.Value.Invoke(message,m.Key);
+                    }
+                    catch (Exception e) { }
+                }
+            }
+            foreach (var r in regexResponses)
+            {
+                if (Regex.IsMatch(message.text, r.Regex) && message.subtype != "bot_message")
+                {
+                    client.PostMessage(null, message.channel, r.Response);
                 }
             }
         }
 
-        private void TemperatureConversion(NewMessage message)
+        private void TemperatureConversion(NewMessage message, string regex)
         {
-            var matching = Regex.Match(message.text, "([+-]?\\d+(\\.\\d+)*)\\s?°[Cc]");
+            var matching = Regex.Match(message.text, regex);
             var groups = new List<string>();
             foreach(Group g in matching.Groups)
             {
                 groups.Add(g.Value);
             }
 
-            var replacement = $"That is {Double.Parse(groups[1]) * 9.0 / 5.0 + 32} in Farenheit";
+            var replacement = $"That is {Double.Parse(groups[1]) * 9.0 / 5.0 + 32} in farenheit";
 
-            var responseText = String.Format("That is {0} in Farenheit", Regex.Replace(message.text, "([+-]?\\d+(\\.\\d+)*)\\s?°[Cc]", delegate (Match match)
+            client.PostMessage(null, message.channel, replacement);
+        }
+
+        private void DistanceConversion(NewMessage message,string regex)
+        {
+            var matching = Regex.Match(message.text, regex);
+            var groups = new List<string>();
+            foreach (Group g in matching.Groups)
             {
-                double temp = Double.Parse(match.Groups[1].ToString());
-                return (temp * 9.0 / 5.0 + 32).ToString();
-            }));
+                groups.Add(g.Value);
+            }
+
+            var replacement = $"That is {Double.Parse(groups[1]) / 0.621371} in miles";
+
             client.PostMessage(null, message.channel, replacement);
         }
     }
---
+++ SlackApp/SlackBot/RegexResponse.cs
@@ -0,0 +1,14 @@
+﻿using SlackAPI.WebSocketMessages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlackApp.SlackBot
+{
+    public class RegexResponse
+    {
+        public string Regex { get; set; }
+        public string Response {get; set;}
+    }
+}
