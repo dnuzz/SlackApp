fa252a83a61cecb59bffdedd39f71ff4abf811dc
dnuzzomueller <dnuzzomueller@cimpress.com>
2018-05-01 20:36:23 -0400

    Adding in interfaces for slack client

---
+++ SlackAPIService/ISlackClient.cs
@@ -0,0 +1,13 @@
+﻿using SlackAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackAPIService
+{
+    public interface ISlackClient
+    {
+        SlackClient GetSlackClient();
+        SlackSocketClient GetSlackSocketClient();
+    }
+}
---
+++ SlackAPIService/SlackClientSerivce.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using SlackAPI;
+
+namespace SlackAPIService
+{
+    public class SlackClientService : ISlackClient
+    {
+        private SlackClient _client;
+        private SlackSocketClient _socketclient;
+
+        public SlackClientService(string authtoken,string socketauthtoken = null)
+        {
+            if (socketauthtoken == null) { socketauthtoken = authtoken; }
+
+                ManualResetEventSlim clientReady = new ManualResetEventSlim(false);
+            SlackClient client = new SlackClient(authtoken);
+            client.Connect((connected) => {
+                // This is called once the client has emitted the RTM start command
+                clientReady.Set();
+            }, () => {
+                Console.WriteLine("Connected Client to Slack");
+            });
+            clientReady.Wait();
+
+            SlackSocketClient socketclient = new SlackSocketClient(socketauthtoken);
+            client.Connect((connected) => {
+                // This is called once the client has emitted the RTM start command
+                clientReady.Set();
+            }, () => {
+                Console.WriteLine("Connected SocketClient to Slack");
+            });
+            clientReady.Wait();
+
+            this._socketclient = socketclient;
+            this._client = client;
+        }
+
+        public SlackClient GetSlackClient()
+        {
+            return _client;
+        }
+
+        public SlackSocketClient GetSlackSocketClient()
+        {
+            return _socketclient;
+        }
+    }
+}
